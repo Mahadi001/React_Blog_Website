672077c5e4004c7ccee40b1fedd6db5a38c63807
Mohammed Mahadi <technuf.aphelia@gmail.com>
2022-11-14 18:38:05 +0600

    navbar created

---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { creatGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = creatGlobalStyle
+`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
+
+html {
+    font-size: 62.5%;
+    /* scroll-behavior: smooth; */
+    /* 1rem = 10px */
+    overflow-x: hidden;   
+}
+
+h1 {
+    color: ${({theme}) => theme.colors.heading};
+    font-size: 6rem;
+    font-weight: 900;
+}
+
+h2 {
+    color: ${({theme}) => theme.colors.heading};
+    font-size: 4.4rem;
+    font-weight: 300;
+    white-space: normal;
+    text-align: center;
+}
+
+h3 {
+    font-size: 1.8rem;
+    font-weight: 400;
+}
+
+p {
+    color: ${({theme}) => theme.colors.text};
+    opacity: .7;
+    font-size: 1.65rem;
+    line-height: 1.5;
+    margin-top: 1rem;
+    font-weight: 400;
+}
+
+a {
+    text-decoration: none;
+}
+
+li {
+    list-style: none;
+}
+
+`
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Navbar from './Navbar'
+import styled from 'styled-components'
 
 const Header = () => {
   return (
-    <header>
+    <Mainheader>
       <NavLink to="/">
-        <img src="./images/logo.svg" alt="logo" />
+        <img src="./images/logo.svg" alt="logo" className='logo' style={{height: 100, width: 200}}/>
       </NavLink>
-    </header>
+      <Navbar/>
+    </Mainheader>
   )
-}
+};
+
+const Mainheader = styled.header `
+  padding: 0 4.8rem;
+  height: 10rem;
+  background-color: ${({ theme }) => theme.colors.bg};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .logo {
+    height: 3rem;
+  }
+`;
 
 export default Header
